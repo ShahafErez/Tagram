cf94a8eb2257e40c00cd15fe47a305982533867a
ShahafErez <Shahaf30@gmail.com>
2022-12-23 00:54:13 +0200

    Annotate tags & relations

---
+++ frontend/src/components/Project/AnnotationTag.js
@@ -0,0 +1,69 @@
+import { TokenAnnotator } from "react-text-annotate";
+import React, { useState } from "react";
+
+export default function AnnotationTag(props) {
+  let TAG_COLORS = {};
+  let tag_options = [];
+
+  props.labels.forEach(function (label, index) {
+    TAG_COLORS[label["name"]] = label["color"];
+    tag_options.push(label["name"] + "");
+  });
+
+  const file = props.file;
+
+  const [value, setValue] = useState([]);
+  const [tag, setTag] = useState(tag_options[0]);
+
+  const handleValueChange = (value) => {
+    setValue(value);
+  };
+
+  const handleTagChange = (e) => {
+    setTag(e.target.value);
+  };
+
+  return (
+    <div class="annotate">
+      <div style={{ padding: "10px" }}>
+        <h4>Annotate Tags</h4>
+        <div>
+          <select
+            class="form-select"
+            size="2"
+            aria-label="multiple select example"
+            onChange={handleTagChange}
+            value={tag}
+          >
+            {/* todo- change to be dyanmic */}
+            {tag_options.map((element, index) => (
+              <option key={index}>{element}</option>
+            ))}
+          </select>
+        </div>
+
+        <div style={{ marginTop: "15px" }}>
+          <TokenAnnotator
+            style={{
+              padding: "5px",
+              lineHeight: 1.5,
+              border: "1px solid #fcc727",
+              borderRadius: "2px",
+            }}
+            tokens={file.text.split(" ")}
+            value={value}
+            onChange={handleValueChange}
+            getSpan={(span) => ({
+              ...span,
+              tag: tag,
+              color: TAG_COLORS[tag],
+            })}
+          />
+        </div>
+      </div>
+
+      <h4>Output Value</h4>
+      <pre>{JSON.stringify(value, null, 2)}</pre>
+    </div>
+  );
+}
--- frontend/src/components/Project/ProjectPage.js
+++ frontend/src/components/Project/ProjectPage.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import MetaTaggingObject from "./MetaTaggingObject";
-import Annotation from "./Annotation";
+import AnnotationTag from "./AnnotationTag";
+import AnnotationRelation from "./AnnotationRelation";
 
 export default function ProjectPage() {
   let { id } = useParams();
@@ -12,8 +12,21 @@ export default function ProjectPage() {
     is_project_manager: false,
     created_at: "",
   });
-  const [metaTaggingLabels, setMetaTaggingLabels] = useState();
+  const [file, setFile] = useState({
+    file: "",
+    project_id: "",
+    text: "",
+  });
 
+  // settings arrays for labels & labels types
+  const [metaTaggingLabels, setMetaTaggingLabels] = useState([]);
+  const [tagsLabels, setTagsLabels] = useState([]);
+  const [relationsLabels, setRelationsLabels] = useState([]);
+
+  const [isAnnotateTags, setIsAnnotateTags] = useState(false);
+  const [isAnnotateRelations, setIsAnnotateRelations] = useState(false);
+
+  // getting project details
   useEffect(() => {
     let meta_tagging_id = "";
 
@@ -29,42 +42,90 @@ export default function ProjectPage() {
         )
       )
       .then((response) => response.json())
-      .then((data) => setMetaTaggingLabels(data));
-
+      .then((data) => {
+        setMetaTaggingLabels(data);
+        let tags = [];
+        let relations = [];
+        data.forEach(function (label, index) {
+          // adding the labels to the correct type
+          if (label["type"] == "Tag") {
+            tags.push(label);
+          }
+          if (label["type"] == "Relation") {
+            relations.push(label);
+          }
+        });
+        setTagsLabels(tags);
+        setRelationsLabels(relations);
+      });
     // we need to put [] as the second argument, if we want to render only once
+  }, []);
+
+  // getting project file
+  useEffect(() => {
+    fetch("/api/project/get-file?project_id=" + id)
+      .then((response) => {
+        if (!response.ok) {
+          console.log("response not OK");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setFile(data);
+      });
   }, []);
 
   return (
     <div
-      class="card"
-      style={{ textAlign: "center", width: "50%", margin: "auto" }}
+      class="card project-page"
+      style={{
+        textAlign: "center",
+        width: "50%",
+        margin: "auto",
+        minHeight: "500px",
+      }}
     >
       <div class="card-body">
         <h2 class="card-title">{project.title}</h2>
-        <p class="card-text">
-          {" "}
-          <b>Project Id:</b> {id}
-        </p>
+
         {project.description != "" && (
-          <p class="card-text">
-            {" "}
-            <b>Description: </b>
-            {project.description}
-          </p>
+          <p class="card-text">{project.description}</p>
         )}
-        <p class="card-text">
-          {" "}
-          <b>Is the manager: </b>
-          {String(project.is_project_manager)}
-        </p>
-        <p class="card-text">
-          {" "}
-          <b>Creation date:</b> {project.created_at}
-        </p>
-      </div>
-      <MetaTaggingObject metaTagging={metaTaggingLabels} />
-      <div>
-        <Annotation project_id={id}></Annotation>
+
+        {metaTaggingLabels.length > 0 && (
+          <MetaTaggingObject metaTagging={metaTaggingLabels} />
+        )}
+
+        <div>
+          <div class="btn-group" role="group" aria-label="Basic example">
+            <button
+              type="button"
+              class="btn btn-outline-primary"
+              onClick={() => {
+                setIsAnnotateTags(true);
+                setIsAnnotateRelations(false);
+              }}
+            >
+              Annotate Tags
+            </button>
+            <button
+              type="button"
+              class="btn btn-outline-primary"
+              onClick={() => {
+                setIsAnnotateRelations(true);
+                setIsAnnotateTags(false);
+              }}
+            >
+              Annotate Relations
+            </button>
+          </div>
+          {tagsLabels.length > 0 && isAnnotateTags && (
+            <AnnotationTag file={file} labels={tagsLabels} />
+          )}
+          {relationsLabels.length > 0 && isAnnotateRelations && (
+            <AnnotationRelation file={file} labels={relationsLabels} />
+          )}
+        </div>
       </div>
     </div>
   );
