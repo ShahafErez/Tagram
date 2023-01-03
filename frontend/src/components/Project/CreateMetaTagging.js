3bc388e3b273476e569f8918d57486c4dc902ea6
ShahafErez <Shahaf30@gmail.com>
2023-01-03 16:12:07 +0200

    changed to create meta tagging page

--- frontend/src/components/Project/CreateMetaTagging.js
+++ frontend/src/components/Project/CreateMetaTagging.js
@@ -3,10 +3,12 @@ import React, { useState } from "react";
 export default function CreateMetaTagging(props) {
   // meta-tagging setting
   const [title, setTitle] = useState("");
+  const [currentMetaModel, setCurrentMetaModel] = useState("");
+  const [metaModelArray, setMetaModelArray] = useState([]);
+  const [isMetaTaggingCreated, setIsMetaTaggingCreated] = useState(false);
 
   // labels settings
-  let options = ["Class", "Attribute", "Aggregation", "Inheritance"];
-  const [labelName, setLabelName] = useState(options[0]);
+  const [labelName, setLabelName] = useState(metaModelArray[0]);
   const [labelType, setLabelType] = useState("");
   const [labelColor, setLabelColor] = useState("#000000");
 
@@ -59,10 +61,17 @@ export default function CreateMetaTagging(props) {
     resetLabelValues(e);
   };
 
+  function setMetaModelInput() {
+    let temp_model_array = JSON.parse(JSON.stringify(metaModelArray));
+    temp_model_array.push(currentMetaModel);
+    setCurrentMetaModel("");
+    setMetaModelArray(temp_model_array);
+  }
+
   function resetLabelValues(e) {
     e.target.reset();
 
-    setLabelName(options[0]);
+    setLabelName(metaModelArray[0]);
     setLabelType("");
     setLabelColor("#000000");
 
@@ -73,22 +82,143 @@ export default function CreateMetaTagging(props) {
     });
   }
 
+  /** Returns the part that chooses the title and meta-model*/
+  function rederMetaTaggingDetails() {
+    return (
+      <div class="p-3 mb-2">
+        <h5>Title</h5>
+        <input
+          class="form-control"
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Enter meta-tagging title"
+        />
+
+        <div style={{ marginTop: "20px" }}>
+          <h5>Create Meta-Model</h5>
+          <label>Please add your meta-model labels</label>
+          <input
+            class="form-control"
+            type="text"
+            required
+            placeholder="Enter meta-model label"
+            onChange={(e) => setCurrentMetaModel(e.target.value)}
+            value={currentMetaModel}
+          />
+          <button
+            type="submit"
+            class="btn btn-secondary"
+            style={{ marginTop: "10px" }}
+            onClick={(e) => setMetaModelInput(e)}
+          >
+            Add Label
+          </button>
+        </div>
+
+        {/* Displaying the current chooices */}
+        <div
+          class="accordion"
+          id="accordionExample"
+          style={{ marginTop: "10px" }}
+        >
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+              <button
+                class="accordion-button"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+                Current added labels in meta-model
+              </button>
+            </h2>
+            <div
+              id="collapseOne"
+              class="accordion-collapse collapse show"
+              aria-labelledby="headingOne"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body">
+                <ul class="list-group">
+                  {metaModelArray.map((element, index) => (
+                    <li class="list-group-item">
+                      <span key={index}>{element}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          style={{ textAlign: "right", marginTop: "15px", marginRight: "15px" }}
+        >
+          <button
+            type="submit"
+            class="btn btn-passive"
+            style={{
+              marginLeft: "10px",
+              backgroundColor: "#adb5bd",
+            }}
+            onClick={() => {
+              props.onBack();
+            }}
+          >
+            Back To Create Project
+          </button>
+          {/* disable if there are no title and meta tagging selected */}
+          {(metaModelArray.length <= 0 || title == "") && (
+            <button
+              type="submit"
+              class="btn btn-primary disabled"
+              style={{ marginLeft: "10px", zIndex: "99999" }}
+            >
+              Save Meta-Model
+            </button>
+          )}
+          {/* enabled */}
+          {metaModelArray.length > 0 && title != "" && (
+            <button
+              type="submit"
+              class="btn btn-primary"
+              style={{ marginLeft: "10px" }}
+              onClick={() => {
+                // setting the label default
+                setLabelName(metaModelArray[0]);
+                setIsMetaTaggingCreated(true);
+              }}
+            >
+              Save Meta-Model
+            </button>
+          )}
+        </div>
+      </div>
+    );
+  }
+
   /** Returns the label selection */
   function renderLabelSelection() {
     return (
       <div class="p-3 mb-2 bg-white text-dark">
         <form onSubmit={handleLabelSubmit}>
-          <h5>Please select one or more labels</h5>
+          <h5 style={{ margintop: "0px" }}>Please select one or more labels</h5>
           <div class="card" style={{ padding: "15px" }}>
             <div>
               {/* Selecting the label type from the meta model list (todo- get from meta-model) */}
               <label>Select label type</label>
               <select
                 class="form-select"
-                onChange={(e) => setLabelName(e.target.value)}
+                onChange={(e) => {
+                  setLabelName(e.target.value);
+                }}
                 aria-label="Default select example"
               >
-                {options.map((element, index) => (
+                {metaModelArray.map((element, index) => (
                   <option key={index}>{element}</option>
                 ))}
               </select>
@@ -197,6 +327,46 @@ export default function CreateMetaTagging(props) {
             </div>
           </div>
         </div>
+
+        <div
+          style={{ textAlign: "right", marginTop: "15px", marginRight: "15px" }}
+        >
+          <button
+            type="submit"
+            class="btn btn-passive"
+            style={{
+              marginLeft: "10px",
+              backgroundColor: "#adb5bd",
+            }}
+            onClick={() => {
+              props.onBack();
+            }}
+          >
+            Back To Create Project
+          </button>
+
+          {/* disable if there are no title and meta tagging selected */}
+          {(labels.length <= 0 || title == "") && (
+            <button
+              type="submit"
+              class="btn btn-primary disabled"
+              style={{ marginLeft: "10px", zIndex: "99999" }}
+            >
+              Save Meta-Tagging
+            </button>
+          )}
+          {/* enabled */}
+          {labels.length > 0 && title != "" && (
+            <button
+              type="submit"
+              class="btn btn-primary"
+              style={{ marginLeft: "10px" }}
+              onClick={createMetaTagging}
+            >
+              Save Meta-Tagging
+            </button>
+          )}
+        </div>
       </div>
     );
   }
@@ -205,60 +375,8 @@ export default function CreateMetaTagging(props) {
     <div class="create-page">
       <h2>Create Meta-Tagging</h2>
 
-      <div class="p-3 mb-2">
-        <h5>Title</h5>
-        <input
-          class="form-control"
-          type="text"
-          required
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="Enter meta-tagging title"
-        />
-      </div>
-
-      {renderLabelSelection()}
-
-      <div
-        style={{ textAlign: "right", marginTop: "15px", marginRight: "15px" }}
-      >
-        <button
-          type="submit"
-          class="btn btn-passive"
-          style={{
-            marginLeft: "10px",
-            backgroundColor: "#adb5bd",
-            width: "80px",
-          }}
-          onClick={() => {
-            props.onBack();
-          }}
-        >
-          Back
-        </button>
-
-        {/* disable if there are no title and meta tagging selected */}
-        {(labels.length <= 0 || title == "") && (
-          <button
-            type="submit"
-            class="btn btn-primary disabled"
-            style={{ marginLeft: "10px", zIndex: "99999" }}
-          >
-            Save Meta-Tagging
-          </button>
-        )}
-        {/* enabled */}
-        {labels.length > 0 && title != "" && (
-          <button
-            type="submit"
-            class="btn btn-primary"
-            style={{ marginLeft: "10px" }}
-            onClick={createMetaTagging}
-          >
-            Save Meta-Tagging
-          </button>
-        )}
-      </div>
+      {!isMetaTaggingCreated && rederMetaTaggingDetails()}
+      {isMetaTaggingCreated && renderLabelSelection()}
     </div>
   );
 }
