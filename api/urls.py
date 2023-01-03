dcc00c8b022ff4e5ef50248d61e18a9d38b630c8
shani <shanirah@post.bgu.ac.il>
2023-01-03 23:21:05 +0200

    changed the paths

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('project/', include('api.project.urls')),
     path('meta-tagging/', include('api.meta_tagging.urls')),
+    path('users/', include('api.users.urls')),
 ]
--- api/users/urls.py
+++ api/users/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from .views import GetSessionDetails
 
 
-# app_name = 'project'
+# app_name = 'users'
 
 
 urlpatterns = [
-    #path('get-all', ProjectView.as_view()),
     path('getsession/', GetSessionDetails.as_view()),
 
    
