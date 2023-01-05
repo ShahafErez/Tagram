2bd2dcb864843a1285facc11b7f484d8fa2a73dc
noashpack <shpack@post.bgu.ac.il>
2023-01-05 21:33:54 +0200

    New users and users&projects models +functins

--- api/users/models.py
+++ api/users/models.py
@@ -1,5 +1,25 @@
+import random
+import string
 from django.db import models
+from api.project.models import Project
+
+def generate_unique_code():  
+    length = 6
+
+    while True:
+        id = ''.join(random.choices(string.ascii_uppercase, k=length))
+        # checking if other project has his id
+        if UsersInProject.objects.filter(project_id=id).count() == 0:
+            break
+    return id
+
 
 class User(models.Model):
-    username = models.CharField(max_length=500)
+    username = models.CharField(max_length=500, unique=True, primary_key=True)
     is_project_manager = models.BooleanField(default=False)
+
+
+class UsersInProject(models.Model):
+    mapping_id = models.CharField(max_length=8, default=generate_unique_code, unique=True, primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
---
+++ api/users/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import Project, User, UsersInProject
+from django import forms
+
+
+class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(allow_blank=False)
+
+    class Meta:
+        model = User
+        fields = ('username', 'is_project_manager')
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(allow_blank=False)
+
+    class Meta:
+        model = User
+        fields = ('username',)
+
+
+class UsersInProjectSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(allow_blank=False)
+    project = serializers.CharField(allow_blank=False)
+
+    class Meta:
+        model = UsersInProject
+        fields = ('mapping_id','user','project')
+
+
+class CreateUsersInProjectSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(allow_blank=False)
+    project = serializers.CharField(allow_blank=False)
+
+    class Meta:
+        model = UsersInProject
+        fields = ('user','project')
