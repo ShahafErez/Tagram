7bd2af1e3b46e82dae0038a88a43da079aa64904
ShahafErez <Shahaf30@gmail.com>
2023-01-06 18:18:55 +0200

    navbar & my projects page

---
+++ frontend/src/components/Project/ProjectPreview.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+export default function ProjectPreview(props) {
+  return (
+    <div class="card">
+      <h5 class="card-title">project title: {props.title}</h5>
+      <p>project id: {props.id}</p>
+      <p>meta tagging id: {props.metaTagging}</p>
+    </div>
+  );
+}
