d2d9bbda081bdd36ae4087817243a77398b11575
shani <shanirah@post.bgu.ac.il>
2023-04-19 22:45:58 +0300

    reset password sent page

--- tagram/urls.py
+++ tagram/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('home', index,  name='home'),
     path('', include('frontend.urls'), name='home'),
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="frontend/password_reset.html"), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="frontend/password_reset_sent.html"), name="password_reset_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
     path('password_reset/', auth_views.PasswordResetView.as_view(
